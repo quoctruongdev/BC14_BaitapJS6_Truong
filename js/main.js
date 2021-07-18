07befc58875e41bd58f953f4aa0d5bc37af4005b
Quoc Truong <tqt.ntml@gmail.com>
2021-07-18 20:49:23 +0700

    add file

--- js/main.js
+++ js/main.js
@@ -2,7 +2,6 @@
 document.getElementById("themSo").addEventListener("click", tinhToan);
 var arrayAll = [];
 function tinhToan() {
-  
   var number = document.getElementById("soNguyen").value;
   var ketQua = document.getElementById("ketQua");
   ketQua.innerHTML = "";
@@ -15,7 +14,7 @@ function tinhToan() {
     pEL.innerHTML = "Các thành phần trong mảng: " + "[" + arrayAll + "]";
   }
   ketQua.appendChild(pEL);
-  $(':input').val('');
+  $(":input").val("");
 }
 
 // Tìm số nhỏ nhất và số nguyên dương nhỏ nhất
@@ -26,16 +25,16 @@ function soMin() {
   ketQua.innerHTML = "";
   var min = Math.min.apply(null, arrayAll);
   spanMin.innerHTML = "Số nguyên nhỏ nhất trong mảng: " + min;
-  var arrayTemp = []
-  for (var j = 0; j < arrayAll.length ; j++){
-    if (arrayAll[j] >  0) {
+  var arrayTemp = [];
+  for (var j = 0; j < arrayAll.length; j++) {
+    if (arrayAll[j] > 0) {
       arrayTemp.push(+arrayAll[j]);
       var minDuong = Math.min.apply(null, arrayTemp);
       spanMin2.innerHTML = "Số nguyên dương nhỏ nhất trong mảng: " + minDuong;
       ketQua.appendChild(spanMin);
       ketQua.appendChild(spanMin2);
     }
-  }  
+  }
 }
 
 $("#soNguyen").click(function () {
@@ -46,6 +45,7 @@ $("#soNguyen").click(function () {
 document.getElementById("tinhTong").addEventListener("click", tinhTong);
 function tinhTong() {
   var spanEL = document.createElement("p");
+  var spanEL2 = document.createElement("p");
   // var ketQua = document.getElementById("ketQua");
   ketQua.innerHTML = "";
   tong = 0;
@@ -55,21 +55,58 @@ function tinhTong() {
       count++;
       tong += arrayAll[i];
       spanEL.innerHTML =
-        "Tổng của: " + ('',count) + " số nguyên dương trong mảng bằng: " + tong;
+        "Tổng của các số nguyên dương trong mảng bằng: " + tong;
+      spanEL2.innerHTML = "Có tất cả " + count + " số nguyên dương trong mảng";
       ketQua.appendChild(spanEL);
+      ketQua.appendChild(spanEL2);
     }
   }
 }
 
 //Clear data
-document.getElementById("clear").addEventListener('click', clearData);
+document.getElementById("clear").addEventListener("click", clearData);
 function clearData() {
-  $(':input').val('');
+  $(":input").val("");
   arrayAll = [];
-  arrayTemp = []
-  ketQua.innerHTML= ""
+  arrayTemp = [];
+  ketQua.innerHTML = "";
 }
 
+// Tìm số chẵn cuối cùng trong mảng
+document.getElementById("timSo").addEventListener("click", findEven);
 
-      
+function findEven() {
+  var evenEL = document.createElement("p");
+  ketQua.innerHTML = "";
+  ketQua.appendChild(evenEL);
+  arrayAll.sort((x, y) => y - x);
+  for (var i = 0; i < arrayAll.length; i++) {
+    if (arrayAll[i] % 2 == 0) {
+      return (evenEL.innerHTML =
+        "Số chẵn cuối cùng trong mảng: " + arrayAll[i]);
+    } else {
+      return (evenEL.innerHTML = "Không tìm thấy số chẵn trong mảng: -1");
+    }
+  }
+}
 
+// function findEven() {
+// var evenEL = document.createElement("p");
+// ketQua.innerHTML = "";
+
+// for (var i = 0; i < arrayAll.length; i++) {
+//     if( arrayAll[i] > 0 && arrayAll[i] % 2 == 0  ){
+//       var even =  arrayAll.lastIndexOf(arrayAll[i]);
+//       evenEL.innerHTML = "Số chẵn cuối cùng trong mảng: " + arrayAll[even];
+//     }
+//     else if (arrayAll[i] < 0 && arrayAll[i] % 2 == 0){
+//       continue
+//     }
+//     else if(arrayAll[i] % 2 != 0){
+//       evenEL.innerHTML ="không thấy"
+//     }
+
+//   }
+//   ketQua.appendChild(evenEL);
+
+// }
