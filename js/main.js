b399fdd27c9dd20f1aca6ee158fc8dd9088feafc
Quoc Truong <tqt.ntml@gmail.com>
2021-07-19 23:13:18 +0700

    addfile

--- js/main.js
+++ js/main.js
@@ -20,9 +20,10 @@ function tinhToan() {
 // Tìm số nhỏ nhất và số nguyên dương nhỏ nhất
 document.getElementById("min").addEventListener("click", soMin);
 function soMin() {
+  var ketQua1 = document.getElementById("ketQua1");
   var spanMin = document.createElement("p");
   var spanMin2 = document.createElement("p");
-  ketQua.innerHTML = "";
+  ketQua1.innerHTML = "";
   var min = Math.min.apply(null, arrayAll);
   spanMin.innerHTML = "Số nguyên nhỏ nhất trong mảng: " + min;
   var arrayTemp = [];
@@ -31,8 +32,8 @@ function soMin() {
       arrayTemp.push(+arrayAll[j]);
       var minDuong = Math.min.apply(null, arrayTemp);
       spanMin2.innerHTML = "Số nguyên dương nhỏ nhất trong mảng: " + minDuong;
-      ketQua.appendChild(spanMin);
-      ketQua.appendChild(spanMin2);
+      ketQua1.appendChild(spanMin);
+      ketQua1.appendChild(spanMin2);
     }
   }
 }
@@ -46,8 +47,8 @@ document.getElementById("tinhTong").addEventListener("click", tinhTong);
 function tinhTong() {
   var spanEL = document.createElement("p");
   var spanEL2 = document.createElement("p");
-  // var ketQua = document.getElementById("ketQua");
-  ketQua.innerHTML = "";
+  var ketQua1 = document.getElementById("ketQua1");
+  ketQua1.innerHTML = "";
   tong = 0;
   count = 0;
   for (var i = 0; i < arrayAll.length; i++) {
@@ -57,8 +58,8 @@ function tinhTong() {
       spanEL.innerHTML =
         "Tổng của các số nguyên dương trong mảng bằng: " + tong;
       spanEL2.innerHTML = "Có tất cả " + count + " số nguyên dương trong mảng";
-      ketQua.appendChild(spanEL);
-      ketQua.appendChild(spanEL2);
+      ketQua1.appendChild(spanEL);
+      ketQua1.appendChild(spanEL2);
     }
   }
 }
@@ -68,8 +69,8 @@ document.getElementById("clear").addEventListener("click", clearData);
 function clearData() {
   $(":input").val("");
   arrayAll = [];
-  arrayTemp = [];
   ketQua.innerHTML = "";
+  ketQua1.innerHTML = "";
 }
 
 // Tìm số chẵn cuối cùng trong mảng
@@ -77,36 +78,104 @@ document.getElementById("timSo").addEventListener("click", findEven);
 
 function findEven() {
   var evenEL = document.createElement("p");
-  ketQua.innerHTML = "";
-  ketQua.appendChild(evenEL);
-  arrayAll.sort((x, y) => y - x);
-  for (var i = 0; i < arrayAll.length; i++) {
-    if (arrayAll[i] % 2 == 0) {
+  ketQua1.innerHTML = "";
+  ketQua1.appendChild(evenEL);
+  for (var i = arrayAll.length; i >= 0; i--) {
+    if ((arrayAll[i] % 2 == 0) & (arrayAll[i] > 0)) {
       return (evenEL.innerHTML =
         "Số chẵn cuối cùng trong mảng: " + arrayAll[i]);
+    }
+  }
+  return (evenEL.innerHTML = "Không tìm thấy số chẵn trong mảng: -1");
+}
+
+// Đổi vị trí 2 số trong mảng
+
+document.getElementById("doiVitri").addEventListener("click", swap);
+function swap(a, b) {
+  var a = document.getElementById("viTri1").value;
+  var b = document.getElementById("viTri2").value;
+  ketQua.innerHTML = "";
+  var swapEL = document.createElement("p");
+  var c = arrayAll[a];
+  arrayAll[a] = arrayAll[b];
+  arrayAll[b] = c;
+  swapEL.innerHTML = "Mảng mới sau khi đổi vị trí: [" + arrayAll + "]";
+  ketQua.appendChild(swapEL);
+}
+//Sắp sếp mảng theo thứ tự tăng dần
+document.getElementById("sapXep").addEventListener("click", sapXep);
+
+function sapXep() {
+  ketQua.innerHTML = "";
+  var sortEL = document.createElement("p");
+  arrayAll.sort(function (a, b) {
+    return a - b;
+  });
+  sortEL.innerHTML = "Mảng mới sau khi sắp xếp: [" + arrayAll + "]";
+  ketQua.appendChild(sortEL);
+}
+
+// Tìm số nguyên tố
+document.getElementById("soNguyenTo").addEventListener("click", soNT);
+function soNT() {
+  var soNTEL = document.createElement("p");
+  ketQua1.innerHTML = "";
+  ketQua1.appendChild(soNTEL);
+  for (var i = 0; i < arrayAll.length; i++) {
+    if (arrayAll[i] === 2) {
+      return (soNTEL.innerHTML =
+        "số nguyên tố đầu tiên trong mảng 1: [ " + arrayAll[i] + " ]");
+    }
+    if (arrayAll[i] < 2) {
+      soNTEL.innerHTML = "Không tìm thấy số nguyên tố trong mảng : -1";
+    }
+    if ((arrayAll[i] >= 3) & (arrayAll[i] % 2 != 0)(arrayAll[i] % 3 != 0)) {
+      soNTEL.innerHTML =
+        "số nguyên tố đầu tiên trong mảng 1: [ " + arrayAll[i] + " ]";
+      return;
     } else {
-      return (evenEL.innerHTML = "Không tìm thấy số chẵn trong mảng: -1");
+      soNTEL.innerHTML = "Không tìm thấy số nguyên tố trong mảng : -1";
     }
   }
 }
 
-// function findEven() {
-// var evenEL = document.createElement("p");
-// ketQua.innerHTML = "";
+document.getElementById("soSanh").addEventListener("click", compare);
+function compare() {
+  ketQua1.innerHTML = "";
+  var amEL = document.createElement("p");
+  var duongEL = document.createElement("p");
+  var amduongEL = document.createElement("p");
+  ketQua1.appendChild(amEL);
+  ketQua1.appendChild(duongEL);
+  ketQua1.appendChild(amduongEL);
+  var soAm = 0;
+  var soDuong = 0;
+  for (var i = 0; i < arrayAll.length; i++) {
+    if (arrayAll[i] < 0) {
+      soAm++;
+    } else if (arrayAll[i] > 0) {
+      soDuong++;
+    }
+    amEL.innerHTML = "Số lượng số âm: " + soAm;
+    duongEL.innerHTML = "Số lượng số dương: " + soDuong;
+  }
+}
 
-// for (var i = 0; i < arrayAll.length; i++) {
-//     if( arrayAll[i] > 0 && arrayAll[i] % 2 == 0  ){
-//       var even =  arrayAll.lastIndexOf(arrayAll[i]);
-//       evenEL.innerHTML = "Số chẵn cuối cùng trong mảng: " + arrayAll[even];
-//     }
-//     else if (arrayAll[i] < 0 && arrayAll[i] % 2 == 0){
-//       continue
-//     }
-//     else if(arrayAll[i] % 2 != 0){
-//       evenEL.innerHTML ="không thấy"
-//     }
-
-//   }
-//   ketQua.appendChild(evenEL);
-
-// }
+//Đếm số nguyên trong mảng
+document.getElementById("soThuc").addEventListener("click", soNguyen);
+function soNguyen() {
+  var soNguyenEL = document.createElement("p");
+  ketQua1.innerHTML = "";
+  ketQua1.appendChild(soNguyenEL);
+  var soNguyen = 0;
+  for (var i = 0; i < arrayAll.length; i++) {
+    if (arrayAll[i] % 1 === 0) {
+      soNguyen++;
+      soNguyenEL.innerHTML = "Số lượng số nguyên: " + soNguyen;
+    }
+    if (arrayAll[i] % 1 != 0) {
+      soNguyenEL.innerHTML = "Số lượng số nguyên: " + soNguyen;
+    }
+  }
+}
